8a066313b78cd84d26410616835c81bc7168107a
salman <salman7official7@gmail.com>
2025-09-13 12:03:22 +0500

    welcome page design

--- client/src/electron/main.js
+++ client/src/electron/main.js
@@ -19,7 +19,7 @@ function createWindow() {
 
   const { width: screenWidth } = screen.getPrimaryDisplay().workAreaSize;
   const windowWidth = 400;
-  const windowHeight = 500;
+  const windowHeight = 700;
 
   mainWindow = new BrowserWindow({
     width: windowWidth,
