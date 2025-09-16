9b6a11690669d0dcdb6dd16fa2f6af4c27cdb364
salman <salman7official7@gmail.com>
2025-09-16 18:03:36 +0500

    some changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,28 @@
 import React from 'react'
 
-import Wellcome from './pages/Wellcom'
 import ChatSection from './pages/ChatSection'
-
 import Signup from './pages/Signup'
 import Signin from './pages/Signin'
 import Wellcome from './pages/Wellcome'
 import { Route, Routes } from 'react-router-dom'
+import Topbar from './components/Topbar'
 
 export default function App() {
   return (
 
     <>
-
-      {/* <Wellcome/> */}
-      <ChatSection/>
-      {/* <Wellcome />
-      <Signup />
-      <Signin /> */}
-
+      {/* <ChatSection/> */}
+  
 
       <Routes>
+        <Topbar/>
         <Route path="/" element={  <Wellcome />} />
         <Route path="/signup" element={ <Signup />} />
         <Route path="/signin" element={<Signin />} />
       </Routes>
     </>
 
-    // /api/v1/auth/login
+
+// electron-tar
   )
 }
