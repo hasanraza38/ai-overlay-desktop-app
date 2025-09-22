cb33acd514a4658c80796d255e037e23624f9152
Muhammad Hasan Raza <92hasanraza689@gmail.com>
2025-09-22 15:52:20 +0500

    updates

--- server/config/safePay.js
+++ server/config/safePay.js
@@ -1,8 +1,10 @@
 import { Safepay } from "@sfpy/node-sdk";
 
-export const safepay = new Safepay({
+ const safepay = new Safepay({
   environment: process.env.SAFEPAY_ENVIRONMENT,
   apiKey: process.env.SAFEPAY_API_KEY,
   v1Secret: process.env.SAFEPAY_SECRET,
   webhookSecret:process.env.SAFEPAY_WEBHOOK_SECRET
 })
+
+export default safepay
